ff1f2150d64c9afc45d9cd957964bde891016c99
winxeater0 <matheus.a.aguiar@hotmail.com>
2026-02-26 12:35:22 -0300

    remove migrations testes e classes desn

--- src/main/resources/db/migration/V2__add_product_constraints.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE product
-    ADD CONSTRAINT uk_product_sku UNIQUE (sku);
-
-CREATE INDEX idx_product_name ON product(name);
--- src/main/resources/db/migration/V4__seed_initial_products.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product (id, sku, name, price, status)
-VALUES
-    (gen_random_uuid(), 'SKU-001', 'Produto Exemplo', 99.90, 'ACTIVE');
